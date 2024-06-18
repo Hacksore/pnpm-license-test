1ceed955a1e276261d7104c037e58672c3d44dd2
Sean Boult <996134+Hacksore@users.noreply.github.com>
2024-06-18 11:14:22 -0500

    Debug

--- apps/web/generate-licenses.js
+++ apps/web/generate-licenses.js
@@ -1,24 +1,41 @@
 import fs from "node:fs";
 import path from "node:path";
 import { execSync } from "child_process";
-console.log("Generating licenses...");
+console.log("Attempting to generate licenses...");
 
-const __dirname = path.dirname(new URL(import.meta.url).pathname);
+try {
+  // root monorepo dir
+  const __dirname = path.dirname(new URL(import.meta.url).pathname);
 
-// create the __generated__ directory
-fs.mkdirSync(path.join("src", "__generated__"), {
-  recursive: true,
-});
+  console.log("Monorepo dir:", __dirname);
 
-// run the command to generate the licenses
-console.log(__dirname);
-const licenseOutput = execSync(
-  `pnpm licenses --filter=app --prod list --json`,
-  { encoding: 'utf-8' }
-);
+  console.log("Creating __generated__ directory if not exists...");
+  // create the __generated__ directory
+  fs.mkdirSync(path.join("src", "__generated__"), {
+    recursive: true,
+  });
 
-// write the string to the licenses.json file
-fs.writeFileSync(
-  path.join(__dirname, "src", "__generated__", "licenses.json"),
-  licenseOutput
-);
+  console.log(
+    "Running command to generate licenses: pnpm licenses",
+  );
+  // run the command to generate the licenses
+  const licenseOutput = execSync(
+    "pnpm licenses --filter=app --prod --no-optional list --json",
+    { encoding: "utf-8" },
+  );
+
+  const parsedLicensesMap = JSON.parse(licenseOutput);
+  console.log(Object.keys(parsedLicensesMap));
+
+  console.log("Writing all the license information to licenses.json file...");
+  // write the string to the licenses.json file
+  fs.writeFileSync(
+    path.join(__dirname, "src", "__generated__", "licenses.json"),
+    licenseOutput,
+  );
+
+  console.log("🥳 Licenses generated successfully!");
+} catch (error) {
+  console.error("Failed to generate licenses:", error);
+}
+
