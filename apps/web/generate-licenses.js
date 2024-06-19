b2daf56c81ee462eb0020fcf52f4bcbf9ae2c5a8
Sean Boult <996134+Hacksore@users.noreply.github.com>
2024-06-19 08:09:54 -0500

    Dont error out

--- apps/web/generate-licenses.js
+++ apps/web/generate-licenses.js
@@ -3,22 +3,18 @@ import path from "node:path";
 import { execSync } from "child_process";
 console.log("Attempting to generate licenses...");
 
-// just a test
+// root monorepo dir
+const __dirname = path.dirname(new URL(import.meta.url).pathname);
+console.log("Monorepo dir:", __dirname);
+
 try {
-  // root monorepo dir
-  const __dirname = path.dirname(new URL(import.meta.url).pathname);
-
-  console.log("Monorepo dir:", __dirname);
-
   console.log("Creating __generated__ directory if not exists...");
   // create the __generated__ directory
   fs.mkdirSync(path.join("src", "__generated__"), {
     recursive: true,
   });
 
-  console.log(
-    "Running command to generate licenses: pnpm licenses",
-  );
+  console.log("Running command to generate licenses: pnpm licenses");
   // run the command to generate the licenses
   const licenseOutput = execSync(
     "pnpm licenses --filter=app --prod --no-optional list --json",
@@ -37,6 +33,11 @@ try {
 
   console.log("🥳 Licenses generated successfully!");
 } catch (error) {
-  console.error("Failed to generate licenses:", error);
-}
+  console.log("Failed to generate licenses:", error);
 
+  // stub with error
+  fs.writeFileSync(
+    path.join(__dirname, "src", "__generated__", "licenses.json"),
+    JSON.stringify({ error: error.message }),
+  );
+}
